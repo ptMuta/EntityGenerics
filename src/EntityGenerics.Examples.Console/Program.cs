35b695d52e7a9b2386f95e756db159097dbaca45
Joona Romppanen <joona.romppanen@gmail.com>
2015-11-23 18:06:38 +0200

    Clean project files

--- src/EntityGenerics.Examples.Console/Program.cs
+++ src/EntityGenerics.Examples.Console/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using EntityGenerics.Examples.Data;
 using EntityGenerics.Examples.Data.Repositories;
 using Microsoft.Data.Entity;
